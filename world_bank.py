daefd92bb5fd15a3b3f4be5590270b1afc1b6987
Snehalbhai Tandel <snehalbhaitandel@Snehalbhais-MBP.attlocal.net>
2018-10-26 17:55:26 -0400

    Return Maximum Urban Population with Country Name. To use limit, collect action has to be performed along with print to return the resulting row(s)

--- world_bank.py
+++ world_bank.py
@@ -7,16 +7,20 @@ spark = SparkSession.builder.master('local').appName('world_bank').getOrCreate()
 df = spark.read.format('csv')\
     .option('inferSchema', 'true')\
     .option('header', 'false')\
-    .load('Data.csv')
+    .load('/Users/snehalbhaitandel/Projects/spark_certification/data/spark-dev-interview-samples/World_Bank_Data.csv')
 
-''' NOTE:::: Remove ',' within the numbers in column to cast it to LongType to perform aggregations'''
-''' Cast column _c1 to DateType and _c10 to LongType'''
+'''  Remove ',' within the numbers in column to cast it to LongType '''
+
 df2 = df.withColumn('Date', f.from_unixtime(unix_timestamp(col('_c1'),'MM/dd/yyyy')).cast(DateType()))\
     .withColumn('Total_Population', f.regexp_replace(df['_c10'], ',', '').cast(LongType()))
 
 ''' Highest urban population - Country having the highest urban population'''
-max_urban = df2.agg(f.max('_c11').alias('max_urban')).show()
-
+max_urban = df2.groupBy(df2['_c0'].alias('Country'))\
+    .agg(f.max('_c11').alias('max_urban'))\
+    .sort(desc('max_urban'))\
+    .show(1)
+    # .limit(1).collect()
+# print(max_urban)
 ''' Most populous Countries - List of countries in the descending order of their population'''
 top_pop = df2.groupBy(col('_c0').alias('Country'))\
     .agg(f.sum(col('Total_Population')).alias('Highest_Population'))\
@@ -29,3 +33,4 @@ top_pop = df2.groupBy(col('_c0').alias('Country'))\
 '''Highest GDP growth - List of Countries with highest GDP growth from 2009 to 2010 in descending order'''
 '''Internet usage grown - Country where Internet usage has grown the most in the past decade'''
 '''Youngest Country - Yearly distribution of youngest Countries'''
+
